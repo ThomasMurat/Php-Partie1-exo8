4fbff13172f07c24e92a81645308c94cfea6a0a2
ThomasMurat <thomasmurat1992@gmail.com>
2020-06-05 15:03:41 +0200

    up exo8

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Php-Partie1-exo8</title>
+    </head>
+    <body>
+    <?php 
+        $add = 3 + 4;
+        $multipliate = 5 * 20;
+        $divide = 45 / 5;
+    ?>
+    <p><?php echo $add ;?></p>
+    <p><?php echo $multipliate ;?></p>
+    <p><?php echo $divide ;?></p>
+    </body>
+</html>
